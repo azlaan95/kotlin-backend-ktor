e2935f8b5a548a57036458e6319fe46dacc1a0bb
Azlaan Khan <azlaan95@gmail.com>
2023-07-26 17:52:07 +0530

    Working on jwt token table

--- src/main/kotlin/com/azlaan95/configureapp/AppRouting.kt
+++ src/main/kotlin/com/azlaan95/configureapp/AppRouting.kt
@@ -2,6 +2,7 @@ package com.azlaan95.configureapp
 
 import com.azlaan95.approuting.authenticateRoute
 import com.azlaan95.approuting.registerRoute
+import com.azlaan95.approuting.tokensRoute
 import com.azlaan95.approuting.usersRoute
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
@@ -15,6 +16,7 @@ fun Application.configureRouting() {
         }
         authenticate("auth-bearer") {
             usersRoute()
+            tokensRoute()
         }
     }
 }

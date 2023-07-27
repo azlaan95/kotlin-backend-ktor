419cef323d4eebd5ee7376b6ff45f05207edf25c
Azlaan Khan <azlaan95@gmail.com>
2023-07-27 14:20:02 +0530

    Foreign Key implementation

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A demonstration of how we can use Kotlin as Backed with Ktor
 This is a Kotlin Gradle project using Ktor framework to create api.
 I am using CIO engine.
 Authetication restriction is also used to demonstrate how to we can implement it.
-There is no Database connectivity yet but have created a Temp Datastore file which will save the data when pushed in it
+I have implemented h2 local database for the connectivity
--- src/main/kotlin/com/azlaan95/database/DatabaseFactory.kt
+++ src/main/kotlin/com/azlaan95/database/DatabaseFactory.kt
@@ -20,8 +20,8 @@ object DatabaseFactory {
     fun init() {
         val driverClassName = "org.h2.Driver"
         val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(url, databaseDriver, databaseUser, databasePassword)
-        //val database = Database.connect(jdbcURL, driverClassName)
+        //val database = Database.connect(url, databaseDriver, databaseUser, databasePassword)
+        val database = Database.connect(jdbcURL, driverClassName)
         transaction(database) {
             SchemaUtils.create(Users, JwtTokens)
         }

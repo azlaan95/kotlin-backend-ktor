c739d77d1c42f44d86e3765b0e70684e7bbe6ca9
Azlaan Khan <azlaan95@gmail.com>
2023-07-28 16:09:27 +0530

    Some issue resolved

--- src/main/kotlin/com/azlaan95/Application.kt
+++ src/main/kotlin/com/azlaan95/Application.kt
@@ -5,14 +5,15 @@ import com.azlaan95.configureapp.configureAuthorization
 import com.azlaan95.configureapp.installPlugins
 import com.azlaan95.database.DatabaseFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.cio.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    DatabaseFactory.init(environment.config)
     installPlugins()
+    DatabaseFactory.init(environment.config)
     configureAuthorization(userDao = AppDependencyInject.userDaoProvider())
     configureRouting()
 }
--- src/main/kotlin/com/azlaan95/configureapp/AppPlugins.kt
+++ src/main/kotlin/com/azlaan95/configureapp/AppPlugins.kt
@@ -14,4 +14,5 @@ fun Application.installPlugins() {
             isLenient = true
         })
     }
+
 }
